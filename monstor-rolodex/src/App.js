06addf5f00c9b73616e48449597e61fb55d5d8e6
gokuljs <gokuljs029@gmail.com>
2021-11-01 18:24:37 +0530

    revise

---
+++ monstor-rolodex/src/App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[{name:'frankestien'},
+                {name:'dracula'},
+                {name:'vampire'}          
+    ]
+    }
+  }
+  render(){
+    return(
+      <div className='App'>
+        {
+          this.state.monsters.map(monster=><h1>{monster.name}</h1>)
+        }
+      </div>
+    )
+  }
+}
+export default App;
