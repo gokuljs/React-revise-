399e6637159c777df08416252e61de13d7b42660
gokuljs <gokuljs029@gmail.com>
2021-11-05 16:53:12 +0530

    add.

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-      hello world
+      <HomePage/>
     </div>
   );
 }
